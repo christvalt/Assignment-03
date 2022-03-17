c37422d361857f728c4ef1c913166e1a0a1a4518
christvalt <valteschrist@gmail.com>
2022-03-17 12:16:22 +0100

    remove failure2

--- src/main/java/es1/controller/messages/DocLoadSuccessMsg.java
+++
@@ -1,6 +0,0 @@
-package controller.messages;
-
-import java.io.Serializable;
-
-public class DocLoadSuccessMsg implements Serializable {
-}
--- src/main/java/es1/controller/messages/StartLoadingMsg.java
+++
@@ -1,6 +0,0 @@
-package controller.messages;
-
-import java.io.Serializable;
-
-public class StartLoadingMsg implements Serializable {
-}
